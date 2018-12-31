3da667ab257d6e90809a655dc42c62254e2dccfa
Jerry Hamlet <jerry@hamletink.com>
2018-12-31 04:48:35 -0800

    Add some documentation and bitlify

--- src/component/empty.js
+++ src/component/empty.js
@@ -1,4 +1,10 @@
 import { always } from 'ramda';
+/**
+ * A simple wrapping component for rendering a null element.
+ * @name EmptyComponent
+ * @returns {null}
+ */
 export const EmptyComponent = always(null);
+
 export default EmptyComponent;
 
